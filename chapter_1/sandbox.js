e0d41b97a8760f5b9718c3b11710a0beb1e74ffa
Trevor Katilius <111081073+trevkat@users.noreply.github.com>
2024-03-29 15:26:31 -0500

    working with variables in JS

---
+++ chapter_1/sandbox.js
@@ -0,0 +1,15 @@
+// variables and logging to the console
+
+//alert('hello!!');
+console.log(1);
+console.log(2);
+
+let age = 25;
+let year = 2024;
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);
