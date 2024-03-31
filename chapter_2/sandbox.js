953f4749209ad04d87aa8b5bc81959e9db583278
Trevor Katilius <111081073+trevkat@users.noreply.github.com>
2024-03-31 09:50:19 -0500

    working with numbers, concatenation, and template strings in JS

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -23,8 +23,8 @@ console.log(fullName.length);
 console.log(fullName.toUpperCase());
 console.log(fullName.toLowerCase());
 
-let result = fullName.toLowerCase();
-console.log(result, fullName);
+//let result = fullName.toLowerCase();
+////console.log(result, fullName);
 
 let index = email.indexOf('@');
 console.log(index);
@@ -45,4 +45,66 @@ console.log(resultFour);
 
 let resultFive = emailTwo.replace('m', 'w'); // updates first character from m to w which will be mario >> wario
 console.log(resultFive);
+
+// ----------------------------------------------------- //
+
+
+// numbers
+
+let radius = 10;
+const pi = 3.14;
+
+console.log(radius, pi);
+
+// math operators +, -, *, /, **, %
+
+//console.log(10 / 2);
+//let result = radius % 3;
+//let result = pi * radius**2;
+
+
+// order of operation - B I D M A S
+
+//let result = 5 * (10-3)**2;
+
+//console.log(result);
+
+// let likes = 10;
+
+// likes = likes + 1;
+// likes++; // same logic as above
+// likes += 10;
+// likes *= 2;
+
+// console.log(likes);
+
+
+// concatenate numbers
+
+// let result = 'the blog has ' + likes + ' likes';
+// console.log(result);
+
+// ----------------------------------------------------- //
+
+// template strings
+const title = 'Best reads of 2024';
+const author = 'mario';
+const likes = 30;
+
+// concatenation way
+//let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
+//console.log(result);
+
+// template string way
+let result = `The blog called ${title} by ${author} has ${likes} likes`;
+console.log(result);
+
+// creating html templates
+let html = `
+    <h2>${title}</h2>
+    <p>By ${author}</p>
+    <span>This blog has ${likes} likes</span>
+`;
+
+    console.log(html);
 
